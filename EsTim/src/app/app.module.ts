a831f05b9c0184ed22cb31bc45fbbe5fedb90c23
Leandro Buzzi <77006961+likahon@users.noreply.github.com>
2023-12-04 22:41:06 -0300

    se modifica

--- EsTim/src/app/app.module.ts
+++ EsTim/src/app/app.module.ts
@@ -7,14 +7,12 @@ import { AppComponent } from './app.component';
 import { GamesComponent } from './modules/games/pages/games/games.component';
 import { HomeModule } from './modules/home/home.module';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsComponent } from './modules/genres/pages/forms/forms.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    GamesComponent,
-    FormsComponent
+    GamesComponent
   ],
   imports: [
     BrowserModule,
