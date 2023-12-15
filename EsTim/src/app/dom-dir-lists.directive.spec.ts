a3d1817ef10515b26b974373f85d5b6088f81bc2
Leandro Buzzi <77006961+likahon@users.noreply.github.com>
2023-12-14 22:45:40 -0300

    se reinicia proyecto

--- EsTim/src/app/dom-dir-lists.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RedOnHoverDirective } from './dom-dir-lists.directive';
-
-describe('DomDirListsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RedOnHoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
